bafd4dbe3556c5f7d2515b009e7ed8a85287022d
wk-j <somnuk.wk@gmail.com>
2018-03-11 02:26:55 +0700

    Buttons

--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,31 @@ import React from "react"
 import { render } from "react-dom"
 
 const NavBar = ({ }) =>
-    <div className="jw-nav">
-        <a className="jw-nav-link">Home</a>
-        <a className="jw-nav-link">Installation</a>
-        <a className="jw-nav-link">Document</a>
-        <a className="jw-nav-link jw-right">Login</a>
+    <div className="nav">
+        <a className="link">Home</a>
+        <a className="link">Installation</a>
+        <a className="link">Document</a>
+        <a className="link right">Login</a>
+    </div>
+
+const Bottons = ({ }) =>
+    <div>
+        <div className="button primary">Primary</div>
+        <div className="button secondary">Secondary</div>
+        <div className="button positive">Positive</div>
+        <div className="button negative">Negative</div>
     </div>
 
 class App extends React.Component {
     render() {
         return (
-            <div>
+            <div className="jw">
                 <NavBar />
                 <div className="message">Message</div>
                 <div className="error">Error</div>
                 <div className="success">Success</div>
                 <div className="warning">Warning</div>
+                <Bottons />
             </div>
         );
     }
