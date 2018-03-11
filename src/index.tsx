5624d7c82137ca2b1757d1a105eba24c537ffa0f
wk-j <somnuk.wk@gmail.com>
2018-03-11 20:32:59 +0700

    Added inverted buttons

--- src/index.tsx
+++ src/index.tsx
@@ -2,38 +2,38 @@ import React from "react"
 import { render } from "react-dom"
 
 const NavBar = ({ }) =>
-    <div className="nav">
-        <a className="link">Home</a>
-        <a className="link">Installation</a>
-        <a className="link">Document</a>
-        <a className="link right">Login</a>
+    <div className="jw nav">
+        <a className="jw nav link">Home</a>
+        <a className="jw nav link">Installation</a>
+        <a className="jw nav link">Document</a>
+        <a className="jw nav link right">Login</a>
     </div>
 
 const Bottons = ({ }) =>
     <div>
-        <div className="button primary">Primary</div>
-        <div className="button secondary">Secondary</div>
-        <div className="button positive">Positive</div>
-        <div className="button negative">Negative</div>
+        <div className="jw button primary">Primary</div>
+        <div className="jw button secondary">Secondary</div>
+        <div className="jw button positive">Positive</div>
+        <div className="jw button negative">Negative</div>
     </div>
 
 const InvertButton = ({ }) =>
     <div>
-        <div className="button invert primary">Primary</div>
-        <div className="button invert secondary">Secondary</div>
-        <div className="button invert positive">Positive</div>
-        <div className="button invert negative">Negative</div>
+        <div className="jw button invert primary">Primary</div>
+        <div className="jw button invert secondary">Secondary</div>
+        <div className="jw button invert positive">Positive</div>
+        <div className="jw button invert negative">Negative</div>
     </div>
 
 class App extends React.Component {
     render() {
         return (
-            <div className="jw">
+            <div>
                 <NavBar />
-                <div className="message">Message</div>
-                <div className="error">Error</div>
-                <div className="success">Success</div>
-                <div className="warning">Warning</div>
+                <div className="jw message">Message</div>
+                <div className="jw error">Error</div>
+                <div className="jw success">Success</div>
+                <div className="jw warning">Warning</div>
                 <Bottons />
                 <InvertButton />
             </div>
